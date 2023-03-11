b3a46a1ac08f0ae0b3069f8ad12138bd057b163d
AlexRasch <46262688+AlexRasch@users.noreply.github.com>
2023-03-11 16:45:15 +0100

    Implemented IRepository

---
+++ OdinCMS.DataAccess/Repository/IRepository/IRepository.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OdinCMS.DataAccess.Repository.IRepository
+{
+    internal interface IRepository<T> where T : class
+    {
+        // Create
+        void Create(T entity);
+        // Read 
+        T GetFirstOrDefault(Expression<Func<T, bool>> filter); 
+        IEnumerable<T> GetAll();
+
+        // Update
+
+        // Delete
+
+    }
+}
