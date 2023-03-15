b2bf89e9ee3a4d9696b4055ce6ae1cc6e79d61e2
AlexRasch <46262688+AlexRasch@users.noreply.github.com>
2023-03-15 18:36:20 +0100

    Index style and swipper

--- OdinCMS/wwwroot/js/site.js
+++ OdinCMS/wwwroot/js/site.js
@@ -2,3 +2,33 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+var swiper;
+
+window.addEventListener('DOMContentLoaded', (event) => {
+
+    fnInitSwipper();
+});
+
+
+function fnInitSwipper() {
+    swiper = new Swiper('.mySwiper', {
+        effect: 'coverflow',
+        grabCursor: true,
+        centeredSlides: true,
+        slidesPerView: 'auto',
+        autoplay: {
+            delay: 5000,
+        },
+        coverflowEffect: {
+            rotate: 0,
+            stretch: 0,
+            depth: 200,
+            modifier: 1,
+            slideShadows: false,
+        },
+        pagination: {
+            el: '.swiper-pagination',
+        },
+        loop: true,
+    });
+}
