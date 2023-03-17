8c8474d43670d9f4a69f32b0c9e682ef426aa685
AlexRasch <46262688+AlexRasch@users.noreply.github.com>
2023-03-17 12:50:03 +0100

    Added roles

--- OdinCMS.Utility/SD.cs
+++ OdinCMS.Utility/SD.cs
@@ -8,6 +8,13 @@ namespace OdinCMS.Utility
 {
     public static class SD
     {
+        /* Customer roles */
+        public const string Role_User_Individual = "Individual";
+        public const string Role_User_Company = "Company";
+
+        /* Manage roles */
+        public const string Role_Admin = "Admin";
+        public const string Role_Employee = "Employee";
 
     }
 }
