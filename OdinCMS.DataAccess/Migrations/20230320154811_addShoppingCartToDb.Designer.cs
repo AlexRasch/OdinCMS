cb7f1be9b90c1b1e60b9bd1306771d64728c8dd8
AlexRasch <46262688+AlexRasch@users.noreply.github.com>
2023-03-20 16:48:40 +0100

    added shopping cart to db

--- OdinCMS.DataAccess/Migrations/20230320154811_addShoppingCartToDb.Designer.cs
+++ OdinCMS.DataAccess/Migrations/20230320154811_addShoppingCartToDb.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using OdinCMS.DataAccess.Data;
 
@@ -11,9 +12,11 @@ using OdinCMS.DataAccess.Data;
 namespace OdinCMS.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230320154811_addShoppingCartToDb")]
+    partial class addShoppingCartToDb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- OdinCms.Models/ShoppingCart.cs
+++ OdinCms.Models/ShoppingCart.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,9 +11,19 @@ namespace OdinCMS.Models
 {
     public class ShoppingCart
     {
+        [Key]
+        public int Id { get; set; }
+
+        // Product
+        public int ProductId { get; set; }
+        [ForeignKey("ProductId"), ValidateNever]
         public Product Product { get; set; }
 
         [Range(1, 100, ErrorMessage = "Please enter a value between 1 and 1000")]
         public int Count { get; set; }
+
+        // User details
+        [ForeignKey("ApplicationUserId")]
+        public ApplicationUser ApplicationUser { get; set; }
     }
 }
