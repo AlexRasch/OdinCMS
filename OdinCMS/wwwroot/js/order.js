67b336c99eed688fdbfb3a73cbf44ba4ba234c43
AlexRasch <46262688+AlexRasch@users.noreply.github.com>
2023-04-04 13:55:24 +0200

    Display all orders

---
+++ OdinCMS/wwwroot/js/order.js
@@ -0,0 +1,37 @@
+﻿var dataTable;
+
+// Entry point
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#tblData').DataTable({
+        "ajax": {
+            "url":"/Admin/Order/GetAll"
+        },
+        "columns": [
+            { "data": "id", "width": "15%" },
+            {
+                "data": "name",
+                "width": "15%"
+            },
+            { "data": "phoneNumber", "width": "15%" },
+            { "data": "applicationUser.email", "width": "15%" },
+            { "data": "orderStatus", "width": "15%" },
+            { "data": "orderTotal", "width": "15%" },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `
+                    <div class="btn-group" role="group">
+                        <a href="/Admin/Order/Detail?orderId=${data}" class="btn btn-primary">Details</a>
+                    </div>
+                    `
+                },
+                "width": "15%"
+            },
+
+        ]
+    });
+}
